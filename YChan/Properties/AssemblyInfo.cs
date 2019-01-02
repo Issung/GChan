fd248e370549775a111415982417eeefcb7b1e29
Ricardo Ribeiro <ricardoribeiro_1991@hotmail.com>
2019-01-02 01:19:28 +0000

    ver bump

--- YChan/Properties/AssemblyInfo.cs
+++ YChan/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.0")]
-[assembly: AssemblyFileVersion("2.6.0")]
+[assembly: AssemblyVersion("2.7.0")]
+[assembly: AssemblyFileVersion("2.7.0")]
