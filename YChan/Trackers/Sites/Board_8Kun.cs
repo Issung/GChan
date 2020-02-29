22bdb7673be8157c90269b5fd1c08a9f1875a11a
Ashai <nekumitsurugi@gmail.com>
2020-02-29 04:53:46 -0800

    Fixed the name 'MessageBox' does not exist in the current context

--- YChan/Trackers/Sites/Board_8Kun.cs
+++ YChan/Trackers/Sites/Board_8Kun.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
 
