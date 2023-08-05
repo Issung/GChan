9a5b991acc97821b749ce65f6b1397b6ff211c7a
Issung <joel@sonofgoran.com>
2023-08-05 13:27:43 +1000

    Generally improve thread safety around operations on Thread/Board tracker lists. Greatly improve race conditions when user adds multiple urls at once

--- GChan/Controls/SortableBindingList/SortableBindingList.cs
+++ GChan/Controls/SortableBindingList/SortableBindingList.cs
@@ -1,15 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing.Imaging;
+using System.Linq;
 
 namespace GChan.Controls
 {
+    /// <summary>
+    /// Binding list that gives list changed events, also supports sorting for the WinForms UI.
+    /// Attempts to be thread-safe as possible within reason.
+    /// </summary>
     public class SortableBindingList<T> : BindingList<T>
     {
         private readonly Dictionary<Type, PropertyComparer<T>> comparers;
         private bool isSorted;
         private ListSortDirection listSortDirection;
         private PropertyDescriptor propertyDescriptor;
+
+        private readonly object ilock = new();
 
         public SortableBindingList()
             : base(new List<T>())
@@ -46,6 +54,32 @@ namespace GChan.Controls
         protected override bool SupportsSearchingCore
         {
             get { return true; }
+        }
+
+        public new void Add(T item)
+        {
+            lock (ilock)
+            {
+                base.Add(item);
+            }
+        }
+
+        public new T this[int index]
+        {
+            get
+            {
+                lock (ilock)
+                {
+                    return base[index];
+                }
+            }
+            set
+            {
+                lock (ilock)
+                {
+                    base[index] = value;
+                }
+            }
         }
 
         protected override void ApplySortCore(PropertyDescriptor property, ListSortDirection direction)
@@ -93,5 +127,89 @@ namespace GChan.Controls
 
             return -1;
         }
+
+        /// <returns>Item removed from <paramref name="index"/>.</returns>
+        public new T RemoveAt(int index)
+        {
+            lock (ilock) 
+            {
+                var item = this[index];
+                base.RemoveAt(index);
+                return item;
+            }
+        }
+
+        public new void Remove(T item)
+        {
+            lock (ilock)
+            {
+                if (Contains(item))
+                {
+                    base.Remove(item);
+                }
+            }
+        }
+
+        /// <returns>Removed items.</returns>
+        public T[] RemoveAll(Predicate<T> match)
+        {
+            lock (ilock)
+            {
+                var itemsToRemove = new List<T>();
+
+                foreach (T item in this)
+                {
+                    if (match(item))
+                    {
+                        itemsToRemove.Add(item);
+                    }
+                }
+
+                foreach (T item in itemsToRemove)
+                {
+                    this.Remove(item);
+                }
+
+                return itemsToRemove.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Lock the list and foreach loop over the items (don't do long operations with this).
+        /// </summary>
+        public void ForEachLocked(Action<T> action)
+        {
+            lock (ilock)
+            { 
+                foreach (var item in this)
+                {
+                    action(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Thread Safe implementation.
+        /// </summary>
+        public List<T> ToList()
+        {
+            lock (ilock)
+            {
+                return Enumerable.ToList(this);
+            }
+        }
+
+        /// <summary>
+        /// Thread Safe implementation.
+        /// </summary>
+        public T[] ToArray()
+        {
+            lock (ilock)
+            {
+                return Enumerable.ToArray(this);
+            }
+        }
+
+        public T[] Copy() => ToArray();
     }
 }
---
+++ GChan/Helpers/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GChan.Helpers
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
